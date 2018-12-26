7bbe32afecee35e19b25ceb33cde97804e1cb826
807255670 <807255670@QQ.COM>
2018-12-26 16:48:41 +0800

    Add files via upload
    实现教师登录功能和显示主页课程

---
+++ OnlineTestSystem Maven Webapp/src/main/java/com/nju/OnlineTestSystem/service/ClassService.java
@@ -0,0 +1,8 @@
+package com.nju.OnlineTestSystem.service;
+
+import java.util.List;
+import com.nju.OnlineTestSystem.model.Class;;
+
+public interface ClassService {
+	public List<Class> getAllClassByTeacherPrimaryKey(Integer id);
+}
